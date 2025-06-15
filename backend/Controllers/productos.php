b7ce467aeb7b81e3f14cc3e859409ba9953081d7
Prodigius <prodigius2138@gmail.com>
2025-06-15 18:06:22 -0300

    huiv

---
+++ backend/Controllers/productos.php
@@ -0,0 +1,19 @@
+<?php
+require "../Models/Producto.php"; // Importar el modelo
+
+$productoModel = new Producto($pdo); // Instancia del modelo
+
+function obtenerProducto() {
+    global $productoModel;
+    echo json_encode($productoModel->obtenerTodos());
+}
+
+function agregarProducto($nombreProducto, $descripcionProducto, $precioProducto, $stockProducto, $categoriaProducto) {
+    global $productoModel;
+    if ($productoModel->agregar($nombreProducto, $descripcionProducto, $precioProducto, $stockProducto, $categoriaProducto)) {
+        echo json_encode(["message" => "producto agregado"]);
+    } else {
+        echo json_encode(["error" => "Error al agregar el producto"]);
+    }
+}
+?>
