906bbb793625d2ceca05dcd6c2c700c48fc4c9b3
emiliano <marqueze145@gmail.com>
2025-11-05 21:08:17 -0300

    mi bdd

--- backend/BBDD/sanitaria_brisas.sql
+++ backend/BBDD/sanitaria_brisas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2025 a las 19:19:26
+-- Tiempo de generación: 06-11-2025 a las 00:58:01
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -18,31 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sanitaria brisas`
+-- Base de datos: `sanitaria_brisas`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `contiene`
---
-
-CREATE TABLE `contiene` (
-  `id_pedido` int(11) NOT NULL,
-  `id_producto` int(11) NOT NULL,
-  `cant_unidades` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `contiene`
---
-
-INSERT INTO `contiene` (`id_pedido`, `id_producto`, `cant_unidades`) VALUES
-(1, 1, 2),
-(1, 3, 50),
-(2, 2, 1),
-(2, 4, 4),
-(2, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -52,20 +29,19 @@ INSERT INTO `contiene` (`id_pedido`, `id_producto`, `cant_unidades`) VALUES
 
 CREATE TABLE `pedido` (
   `id_pedido` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
   `fecha` date NOT NULL,
   `estado` varchar(50) DEFAULT NULL,
-  `precio_total` decimal(10,2) NOT NULL,
-  `direccion_envio` varchar(255) DEFAULT NULL
+  `direccion_envio` varchar(255) DEFAULT NULL,
+  `precio_total` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `pedido`
 --
 
-INSERT INTO `pedido` (`id_pedido`, `id_usuario`, `fecha`, `estado`, `precio_total`, `direccion_envio`) VALUES
-(1, 1, '2025-07-18', 'Entregado', 1602.50, 'Ruta 8, km 23, Deposito A'),
-(2, 2, '2025-07-20', 'Pendiente', 2332.00, 'Camino al Paso Molino 456');
+INSERT INTO `pedido` (`id_pedido`, `id_usuario`, `fecha`, `estado`, `direccion_envio`, `precio_total`) VALUES
+(2, 2, '2025-08-26', 'Enviado', 'Av. Italia 456, Montevideo', 4800.00);
 
 -- --------------------------------------------------------
 
@@ -76,26 +52,25 @@ INSERT INTO `pedido` (`id_pedido`, `id_usuario`, `fecha`, `estado`, `precio_tota
 CREATE TABLE `producto` (
   `id_producto` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `categoria` varchar(50) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `stock` int(11) DEFAULT 0,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `imagen` varchar(255) DEFAULT NULL
+  `stock` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `imagenes` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id_producto`, `nombre`, `categoria`, `precio`, `stock`, `descripcion`, `imagen`) VALUES
-(1, 'Cemento Portland 25kg', 'Materiales Pesados', 320.00, 200, NULL, NULL),
-(2, 'Arena Fina m3', 'Áridos', 950.00, 150, NULL, NULL),
-(3, 'Ladrillo Común', 'Mampostería', 12.50, 5000, NULL, NULL),
-(4, 'Hierro 8mm', 'Metales', 180.00, 300, NULL, NULL),
-(5, 'Pintura Blanca 4L', 'Pinturas', 750.00, 60, NULL, NULL),
-(6, 'clavo', 'construccion', 10.00, 1000, NULL, NULL),
-(7, 'clavo', 'construccion', 10.00, 1000, NULL, NULL),
-(8, 'chapa', 'construccion', 1200.00, 50, NULL, NULL);
+INSERT INTO `producto` (`id_producto`, `nombre`, `categoria`, `precio`, `stock`, `descripcion`, `imagenes`) VALUES
+(14, 'llave inglesa', 'herramientas', 50.00, 10, 'ejemplo', '../assets/pintura.jpeg'),
+(15, 'Pintura', 'Pinturas', 70.00, 20, 'pintura blanca', '../assets/pintura.jpeg'),
+(16, 'Techo', 'Techos', 20.00, 20, 'Techo prueba', '../assets/pintura.jpeg'),
+(17, 'ladrillos', 'Construccion', 700.00, 30, 'construccion prueba', '../assets/pintura.jpeg'),
+(18, 'Pintura en oferta', 'Ofertas', 10.00, 10, 'Pintura en oferta', '../assets/pintura.jpeg'),
+(19, 'pintura', 'Pinturas', 40.00, 20, 'pintura descripcion', '../assets/pintura.jpeg'),
+(23, 'producto', 'Herramientas', 1.00, 22, 'prueba', '../assets/pintura.jpeg');
 
 -- --------------------------------------------------------
 
@@ -108,27 +83,23 @@ CREATE TABLE `usuario` (
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `telefono` varchar(20) DEFAULT NULL,
-  `contraseña` varchar(100) NOT NULL
+  `contraseña` varchar(255) NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `telefono`, `contraseña`) VALUES
-(1, 'Carlos Méndez', 'carlos@barraca.com', '098112233', 'clave123'),
-(2, 'Laura Díaz', 'laura@barraca.com', '099223344', 'segura456');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `telefono`, `contraseña`, `admin`) VALUES
+(1, 'Administrador', 'admin@gmail.com', NULL, 'sanitariabrisas23', 1),
+(2, 'María López', 'marialopez@gmail.com', '092345678', 'hashed456', NULL),
+(3, 'Carlos Silva', 'csilva@gmail.com', '093456789', '$2y$10$3bYro.Hs/9Mu4/khxkK7mOorQWZXOUsMQPwxZ4fuaVvHrj/0U21xu', NULL),
+(5, 'emiliano', 'marquez@gmail.com', '098709908', '$2y$10$PdT3GTdPGREZOsNV4mo28uwR/oKKt6fQ0Liadgrgl5WSKLRuTZ3/e', NULL);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `contiene`
---
-ALTER TABLE `contiene`
-  ADD PRIMARY KEY (`id_pedido`,`id_producto`),
-  ADD KEY `id_producto` (`id_producto`);
 
 --
 -- Indices de la tabla `pedido`
@@ -158,30 +129,23 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `contiene`
---
-ALTER TABLE `contiene`
-  ADD CONSTRAINT `contiene_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`),
-  ADD CONSTRAINT `contiene_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
 
 --
 -- Filtros para la tabla `pedido`
